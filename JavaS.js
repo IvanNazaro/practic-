105d0461605e6842b85e03375f1ac9c881dc9382
IVANNAZAROV <65441189+IvanNazaro@users.noreply.github.com>
2020-10-11 18:21:39 +0300

    progress

--- JavaS.js
+++ JavaS.js
@@ -1,69 +1,71 @@
-let numberOfFilms;
 
-
-function start() {
-    numberOfFilms = +prompt('сколько фильмов вы уже посмотрели ?', '');
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('сколько фильмов вы уже посмотрели ?', '');
-
-    }
-}
-//start();
 
 let personMove = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    private: false
-
+    private: false,
+    start: function() {
+        personMove.count = +prompt('сколько фильмов вы уже посмотрели ?', '');
+        while (personMove.count  == '' || personMove.count  == null || isNaN(personMove.count )) {
+            personMove.count  = +prompt('сколько фильмов вы уже посмотрели ?', '');
+    
+        }
+    },
+    remembeFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt('Один из последних просмотреных фильмов', '');
+            let b = prompt('На сколько оценете его ?', '');
+    
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personMove.movies[a] = b;
+                console.log('DONE');
+            } else {
+                console.log('Error');
+                i--;
+            }
+    
+        }
+    },
+   detectPersonLevel: function() {
+        if (personMove.count < 10) {
+            console.log('Посмотрите больше фильмов');
+        } else if (personMove.count >= 10 && personMove.count < 30) {
+            console.log('ВЫ любитель');
+        } else if (personMove.count > 30) {
+            console.log('вы батюшка киноман');
+        }
+    
+    },
+    showMyDB: function (hidden) {
+        if (hidden == false) {
+            console.log(personMove);
+        }
+    },toggleVersibleMyDB:function(){
+        if(personMove.private ){
+            personMove.private = false;
+        }else{
+            personMove.private = true;
+        }
+    },
+    writeGenres:function() {
+        for (let i = 1; i <= 3; i++) {
+            let gfilm = prompt(`Ваш любимый жанр под номером ? ${i}`, '').toLowerCase();
+            if (gfilm != '' && gfilm != null) {
+                personMove.genres[i - 1] = gfilm;
+            } else {
+                console.log('Вы ввели некоректные данные');
+                i--;
+          }
+    
+        }
+        personMove.genres.forEach((item , i) => {
+            console.log(`Любимый жанр ${i + 1} - Это ${item}`);
+   });
+    },
+    
 };
+console.log(personMove.showMyDB());
 
-function remembeFilms() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt('Один из последних просмотреных фильмов', '');
-        let b = prompt('На сколько оценете его ?', '');
 
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personMove.movies[a] = b;
-            console.log('DONE');
-        } else {
-            console.log('Error');
-            i--;
-        }
-
-    }
-}
-//remembeFilms();
-
-function detectPersonLevel() {
-    if (personMove.count < 10) {
-        console.log('Посмотрите больше фильмов');
-    } else if (personMove.count >= 10 && personMove.count < 30) {
-        console.log('ВЫ любитель');
-    } else if (personMove.count > 30) {
-        console.log('вы батюшка киноман');
-    }
-
-}
-//detectPersonLevel()
-
-function showMyDB(hidden) {
-    if (hidden == false) {
-        console.log(personMove);
-    }
-}
-showMyDB(personMove.private);
-
-function writeGenres() {
-    for (let i = 1; i <= 3; i++) {
-        let gfilm = prompt(`Ваш любимый жанр под номером ? ${i}`, '');
-        if (gfilm != '' && gfilm != null) {
-            personMove.genres[i - 1] = gfilm;
-        } else {
-            i--;
-        }
-
-    }
-}
-writeGenres()
